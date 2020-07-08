5c77387488c4e546cc5182b9b1a958db1f1adab1
cao <mq-clj@xtyyoa.com>
2020-07-08 10:23:11 +0800

    Revert "1111"
    This reverts commit 38f479df4fcea4f2587544045f8f5564158ba039.

--- models/plate.py
+++ models/plate.py
@@ -1,10 +1,10 @@
 
 from datetime import datetime
 from database import db
-#2222
 #33333
+#44444
 class Plate(db.Model):
-    __tablename__ = 'plate2222'
+    __tablename__ = 'plate1111'
 
     id = db.Column(db.Integer, primary_key=True, index=True, nullable=False, autoincrement=True)
     name = db.Column(db.String(20), nullable=False)
